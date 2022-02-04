817ed1bfd281e5062692bea3e4254e77b77c896b
M4K4R <37383837+M4K4R@users.noreply.github.com>
2022-02-04 14:52:32 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,30 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 console.log("Hello World");
 
+app.get ("/", (request, response) => {
+  response.send("Hello Express")
+});
+
+app.get ("/", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html')
+});
+
+app.use('/public', express.static(__dirname + '/public'));
+
+let message = {"message": "Hello json"}
+app.get('/json', (request, response) => {
+  response.json(message)
+});
+
+app.get("/json", (request, response) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    response.json ({"message": "HELLO JSON"})
+  }else{
+    response.json(message)
+  }
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
